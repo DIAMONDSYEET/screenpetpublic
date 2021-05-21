e1d24d1c8158d16005ed3e9c84b115796c1df881
DIAMONDSYEET <73312696+DIAMONDSYEET@users.noreply.github.com>
2021-05-21 07:55:27 -0500

    Update screenpet.py

--- screenpet.py
+++ screenpet.py
@@ -2,5 +2,12 @@ from tkinter import HIDDEN, NORMAL, Tk, Canvas
 root = Tk()
 c = Canvas(root, width=400, height=400)
 c.configure(bg="dark blue", highlightthickness=0)
+c.body_color = "SkyBlue1"
+#time to draw stuf
+#body
+body = c.create_oval(35, 20, 365, 350, outline=c.body_color, fill=c.body_color)
+#ears
+ear_left = c.create_polygon(75, 80, 75, 10, 165, 70, outline=c.body_color, fill=c.body_color)
+ear_right = c.create_polygon(255, 45, 325, 10, 320, 70, outline=c.body_color, fill=c.body_color)
 c.pack()
 root.mainloop()
